2e9759e194d925d227b83ba9ce63396350f83cb2
MetalB <metalb311@gmail.com>
2022-11-14 12:38:17 -0700

    Got all songs showing, but in 1 line. Also stopsong up and running

--- client/index.js
+++ client/index.js
@@ -2,38 +2,27 @@ const form = document.querySelector('form')
 const div1 = document.querySelector('div1')
 const getsong = document.querySelector('#getsong')
 const randAudio = document.querySelector('#randAudio')
+const stopsong = document.querySelector('#stopsong')
+const allsongs = document.querySelector('#allsongs')
 
-songArray = ['01_Laid_to_Rest.mp3', '01_Walk_With_Me_In_Hell.mp3' ]
+songArray = ['01_Laid_to_Rest.mp3', '01_Walk_With_Me_In_Hell.mp3', '02_Nothing_Left.m4a', '04_11th_Hour.mp3', '05_Fake_Messiah.mp3', '05_New_Colossal_Hate.m4a', '08_Through_Struggle.mp3', '09_The_Beginning.mp3' ]
 
 const getSong = () => {
-    // let songs = document.querySelectorAll('audio')
-        
-        // autoplay.songs
         let randomSongFilePath = songArray[Math.floor(Math.random() * songArray.length)]
     console.log(randomSongFilePath)
 randAudio.src = randomSongFilePath
     randAudio.play()
+    alert(randomSongFilePath)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const stopSong = () => {
+    randAudio.pause()
+}
 
 
 function showAllSongs (event) {
     event.preventDefault()
-    
+    document.querySelector("#allsongs").innerHTML = songArray;
 }
 
 
@@ -53,5 +42,6 @@ function addSong (event) {
 
 
 getsong.addEventListener('click', getSong)
-div1.addEventListener('click', showAllSongs)
+stopsong.addEventListener('click', stopSong)
+allsongs.addEventListener('click', showAllSongs)
 form.addEventListener('submit', addSong)
